fe893664b70de95d0c8402235360a7e3020a9a72
PaulaSaffioti <saffiotipaula@gmail.com>
2016-09-05 00:55:01 -0300

    script para correr tests

--- ApplicationServer/tests/entrypoint.sh
+++
@@ -1,2 +0,0 @@
-resttest.py http://127.0.0.1:8000 integration.yaml
-
--- ApplicationServer/tests/run_test.sh
+++ ApplicationServer/tests/run_test.sh
@@ -1,2 +1,15 @@
-docker build -t applicationservertest .
-docker run -it applicationservertest bash
+#!/bin/bash
+
+sudo docker build -t appserverimg ..;
+sudo docker run --name appserver -p 8000:8000 appserverimg &
+
+sudo docker build -t testingappserverimg .;
+sudo docker run --name testingappserver --link appserver:appserverhost -ti testingappserverimg;
+
+sudo docker stop appserver;
+sudo docker stop testingappserver;
+
+#sudo docker rm appserver;
+#sudo docker rm testingappserver;
+#sudo docker rmi appserverimg;
+#sudo docker rmi testingappserverimg;
