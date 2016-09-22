0c98886cb50d9268addea371c68c2237a4c38cd4
Milena <milenafarotto@gmail.com>
2016-09-22 02:13:42 -0300

    Se agregan pruebas que a la vez son parte de la implementación del guardado de usuarios en la base de datos

--- ApplicationServer/src/tests/testing_ABMUsers.cpp
+++ ApplicationServer/src/tests/testing_ABMUsers.cpp
@@ -5,6 +5,45 @@
 #include "leveldb/db.h"
 #include <iostream>
 
+class ExistsUserDBTest : public ::testing::Test { 
+protected:
+	virtual void SetUp() {
+
+		leveldb::Options op;
+		op.create_if_missing = true;
+		leveldb::Status s = leveldb::DB::Open(op, "/tmp/testingABMUser_ExistUser", &db_);
+		Json::Value user;
+		user["id"] = 2;
+		user["first_name"] = "Carlos";
+		user["last_name"] = "Rodriguez";
+		user["email"] = "crodriguez@gmail.com";
+		user["date_of_birth"] = "01/01/1990";
+		user["city"] = "CABA";
+		user["profile_picture"] = "";
+		user["summary"] = "Me gusta el arrte";
+		Json::FastWriter fastWriter;
+		std::string output = fastWriter.write(user);
+		db_->Put(leveldb::WriteOptions(), "user_1", output);
+
+
+	}
+
+	leveldb::DB* db_;
+
+
+	virtual void TearDown() { 
+		delete db_; 
+	}
+};
+/// Get  User that exists already
+TEST_F(ExistsUserDBTest, GetUser) {
+	std::string value;
+	leveldb::Status s = db_->Get(leveldb::ReadOptions(), "user_1", &value);
+	//std::cout << s.ToString();
+	EXPECT_EQ(s.ok(), true);
+
+}
+
 TEST(KeyFound, KeyNotStored) {
 
 ///Error when asking fot user that doesn't exist
@@ -29,7 +68,8 @@ TEST(SaveUser, NotExists) {
 
 
 /// Save New User that already exists
-TEST(ExistsUserDB, SaveUser) {
+/*
+TEST_F(ExistsUserDBTest, SaveUser) {
 	EXPECT_EQ(1,1);
 
 }
@@ -41,11 +81,7 @@ TEST(GetUser, NotExists) {
 
 }
 
-/// Get  User that exists already
-TEST(ExistsUserDB, GetUser) {
-	EXPECT_EQ(1,1);
 
-}
 
 /// Delete User that doesn't exist already
 TEST(DeleteUser, NotExists) {
@@ -54,36 +90,10 @@ TEST(DeleteUser, NotExists) {
 }
 
 /// Delete User that exists already
-TEST(ExistsUserDB, DeleteUser) {
+TEST_F(ExistsUserDBTest, DeleteUser) {
     EXPECT_EQ(1,1);
 
 }
 
-class ExistsUserDB : public testing::Test { 
-protected:
-	ExistsUserDB() {
-
-		leveldb::Options op;
-		op.create_if_missing = true;
-		leveldb::Status s = leveldb::DB::Open(op, "/tmp/testingABMUser_ExistUser", &db_);
-		Json::Value user;
-		user["id"] = 2;
-		user["first_name"] = "Carlos";
-		user["last_name"] = "Rodriguez";
-		user["email"] = "crodriguez@gmail.com";
-		user["date_of_birth"] = "01/01/1990";
-		user["city"] = "CABA";
-		user["profile_picture"] = "";
-		user["summary"] = "Me gusta el arrte";
-
-
-	}
-
-	leveldb::DB* db_;
-	
-
-	virtual ~ExistsUserDB() { 
-		delete db_; 
-	}
-};
+*/
 
