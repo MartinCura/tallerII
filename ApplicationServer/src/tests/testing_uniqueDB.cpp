82e909ff4e2b5ac34ed7ccaf2fb591bcc60a96b7
Milena <milenafarotto@gmail.com>
2016-09-23 17:52:49 -0300

    Se agregan tests para comprobar que la unica instancia de leveldb funcione y para comprobar que el id inicial esté bien seteado

---
+++ ApplicationServer/src/tests/testing_uniqueDB.cpp
@@ -0,0 +1,23 @@
+//
+// Created by milena on 23/09/16.
+//
+
+#include <gtest/gtest.h>
+#include <leveldb/db.h>
+#include "../DB/ApplicationSDB.h"
+
+TEST(ApplicationSDB, getLastID) {
+    leveldb::DB* db = ApplicationSDB::getInstance();
+    std::string result;
+    db->Get(leveldb::ReadOptions(), "lastID", &result);
+
+    EXPECT_EQ(std::stoi(result),0);
+
+    db->Get(leveldb::ReadOptions(), "Key", &result);
+    delete db;
+}
+
+TEST(ApplicationSDB, startDB) {
+    leveldb::DB* db = ApplicationSDB::getInstance();
+    EXPECT_EQ(db != nullptr, true);
+}
