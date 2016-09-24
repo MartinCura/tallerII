5744da1fc5f415785f5cf7554eb7df8b2ad46dd6
PaulaSaffioti <saffiotipaula@gmail.com>
2016-09-24 17:12:53 -0300

    agrego posibilidad de leer un archivo, con su correspondiente manejo de errores. Por el momento no devuelve el archivo.

--- ApplicationServer/src/api/handler/PictureHandler.cpp
+++ ApplicationServer/src/api/handler/PictureHandler.cpp
@@ -5,7 +5,13 @@ PictureHandler::PictureHandler() {}
 PictureHandler::~PictureHandler() {}
 
 Response* PictureHandler::handleGetRequest(http_message* httpMessage, string url) {
-    return this->getNotImplementedResponse();
+    try {
+        int userId = this->getUserIdFromUrl(url);
+        Response* response = this->buildGetPictureResponse(userId);
+        return response;
+    } catch (InvalidRequestException& e) {
+        return this->getBadRequestResponse(e.getMessage());
+    }
 }
 
 Response* PictureHandler::handlePostRequest(http_message* httpMessage) {
@@ -18,4 +24,26 @@ Response* PictureHandler::handleDeleteRequest(http_message* httpMessage) {
 
 Response* PictureHandler::handlePutRequest(http_message* httpMessage) {
     return this->getNotImplementedResponse();
+}
+
+Response* PictureHandler::buildGetPictureResponse(int id) {
+    string path = "../ApplicationServer/img/profile/" + to_string(id) + ".jpg";
+    vector<char> buffer;
+    FILE* stream = fopen(path.c_str(), "rb");
+    if (stream == NULL) {
+        throw InvalidRequestException("Cannot find requested picture");
+    }
+    fseek(stream, 0, SEEK_END);
+    unsigned long length = (unsigned long) ftell(stream);
+    rewind(stream);
+    buffer.resize(length);
+    int lengthRead = fread(&buffer[0], 1, length, stream);
+    fclose(stream);
+    if (lengthRead == 0) {
+        throw InvalidRequestException("Failed to open requested picture");
+    }
+    Response* response = new Response();
+    response->setSuccessfulHeader();
+    response->setSuccessfulBody();
+    return response;
 }
--- ApplicationServer/src/api/handler/PictureHandler.h
+++ ApplicationServer/src/api/handler/PictureHandler.h
@@ -1,6 +1,7 @@
 #ifndef PROJECT_PICTUREHANDLER_H
 #define PROJECT_PICTUREHANDLER_H
 
+#include <fstream>
 #include "Handler.h"
 
 class PictureHandler : public Handler {
@@ -12,6 +13,9 @@ public:
     Response* handleGetRequest(http_message* httpMessage, string url);
     Response* handleDeleteRequest(http_message* httpMessage);
     Response* handlePutRequest(http_message* httpMessage);
+
+private:
+    Response* buildGetPictureResponse(int id);
 };
 
 #endif //PROJECT_PICTUREHANDLER_H
