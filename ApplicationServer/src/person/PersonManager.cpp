974e977bd356ec7498672730f324b56983e97a35
PaulaSaffioti <saffiotipaula@gmail.com>
2016-10-01 17:24:07 -0300

    Agrego validación de que se envie la password

--- ApplicationServer/src/person/PersonManager.cpp
+++ ApplicationServer/src/person/PersonManager.cpp
@@ -113,9 +113,9 @@ long PersonManager::savePerson(Json::Value person_json) {
     long user_id;
     Json::FastWriter fastWriter;
 
-    if (!person_json.isMember("email")) {
-        throw InvalidRequestException("Missing email");
-    }
+    if (!person_json.isMember("email")) throw InvalidRequestException("Missing email");
+    if (!person_json.isMember("password")) throw InvalidRequestException("Missing password");
+
     user_mail=  person_json["email"].asString();
     user_id = person_json["id"].asLargestInt();
 
