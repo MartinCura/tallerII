aa2376992bcaa154573ebad61cb3e87e0340e229
PaulaSaffioti <saffiotipaula@gmail.com>
2016-10-03 18:07:15 -0300

    metadata con cantidad total de usuarios en GET de allusers

--- ApplicationServer/src/api/handler/AllUsersHandler.cpp
+++ ApplicationServer/src/api/handler/AllUsersHandler.cpp
@@ -12,6 +12,7 @@ Response* AllUsersHandler::handleGetRequest(http_message* httpMessage, string ur
     Json::Value responseBody;
     responseBody["all_users"];
     std::vector<long>* ids = personManager->getAllUsersIds();
+    responseBody["metadata"]["count"] = ids->size();
     for (vector<long>::iterator iter = ids->begin() ; iter != ids->end() ; iter++) {
         responseBody["all_users"].append(*iter);
     }
