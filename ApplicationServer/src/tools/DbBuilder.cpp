8978844df8817bda05fb64757abc90e1999d8ea6
Milena <milenafarotto@gmail.com>
2016-11-03 03:02:23 -0300

    se cambia la pass

--- ApplicationServer/src/tools/DbBuilder.cpp
+++ ApplicationServer/src/tools/DbBuilder.cpp
@@ -73,7 +73,7 @@ Person* DbBuilder::getFakePerson2() {
     person->setFirstName("Jane");
     person->setLastName("Doe");
     person->setEmail("jane@doe.com");
-    person->setPassword("123");
+    person->setPassword("123abc");
     person->setDateOfBirth("01/11/1991");
     person->setCity("Madrid");
     person->setSummary("Hi, this is Jane Doe");
@@ -114,7 +114,7 @@ Person* DbBuilder::getFakePerson3() {
     person->setFirstName("Carlos");
     person->setLastName("Rodriguez");
     person->setEmail("crodriguez@gmail.com");
-    person->setPassword("123");
+    person->setPassword("123abc");
     person->setDateOfBirth("01/01/1990");
     person->setCity("CABA");
     person->setSummary("Hola, soy Carlos Rodriguez");
